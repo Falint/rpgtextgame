00e26c47933adba00ac6794989b25fd5a18c47fa
Tenyom <kafkaxz2234@gmail.com>
2026-04-27 18:41:25 +0700

    TEST

---
+++ build/CMakeFiles/textrpg.dir/link.d
@@ -0,0 +1,142 @@
+textrpg: \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o \
+  CMakeFiles/textrpg.dir/src/main.cpp.o \
+  CMakeFiles/textrpg.dir/src/domain/inventory.cpp.o \
+  CMakeFiles/textrpg.dir/src/domain/enemy.cpp.o \
+  CMakeFiles/textrpg.dir/src/domain/player.cpp.o \
+  CMakeFiles/textrpg.dir/src/data/monsters.cpp.o \
+  CMakeFiles/textrpg.dir/src/data/weapons.cpp.o \
+  CMakeFiles/textrpg.dir/src/data/items.cpp.o \
+  CMakeFiles/textrpg.dir/src/data/upgrades.cpp.o \
+  CMakeFiles/textrpg.dir/src/game/item_service.cpp.o \
+  CMakeFiles/textrpg.dir/src/game/battle_service.cpp.o \
+  CMakeFiles/textrpg.dir/src/game/shop_service.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/terminal.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/renderer.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/screens.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/screens2.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/screens3.cpp.o \
+  CMakeFiles/textrpg.dir/src/tui/app.cpp.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libstdc++.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so \
+  /usr/lib/libm.so.6 \
+  /usr/lib/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so \
+  /usr/lib/libc.so.6 \
+  /usr/lib/libc_nonshared.a \
+  /usr/lib/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o \
+  /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o \
+  /usr/lib/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crti.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtbeginS.o:
+
+CMakeFiles/textrpg.dir/src/main.cpp.o:
+
+CMakeFiles/textrpg.dir/src/domain/inventory.cpp.o:
+
+CMakeFiles/textrpg.dir/src/domain/enemy.cpp.o:
+
+CMakeFiles/textrpg.dir/src/domain/player.cpp.o:
+
+CMakeFiles/textrpg.dir/src/data/monsters.cpp.o:
+
+CMakeFiles/textrpg.dir/src/data/weapons.cpp.o:
+
+CMakeFiles/textrpg.dir/src/data/items.cpp.o:
+
+CMakeFiles/textrpg.dir/src/data/upgrades.cpp.o:
+
+CMakeFiles/textrpg.dir/src/game/item_service.cpp.o:
+
+CMakeFiles/textrpg.dir/src/game/battle_service.cpp.o:
+
+CMakeFiles/textrpg.dir/src/game/shop_service.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/terminal.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/renderer.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/screens.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/screens2.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/screens3.cpp.o:
+
+CMakeFiles/textrpg.dir/src/tui/app.cpp.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libstdc++.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libm.so:
+
+/usr/lib/libm.so.6:
+
+/usr/lib/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libc.so:
+
+/usr/lib/libc.so.6:
+
+/usr/lib/libc_nonshared.a:
+
+/usr/lib/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/libgcc.a:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/crtendS.o:
+
+/usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/crtn.o:
+
+/usr/lib/ld-linux-x86-64.so.2:
